d38af91a1e5c10c6473f396811ea8f7427393b88
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2023-02-19 02:41:15 +0400

    feat: get subscribed active posts query added

--- db/query/content.sql
+++ db/query/content.sql
@@ -1,1 +1,69 @@
 -- name: GetSubscribedActivePosts :many
+SELECT pc.id,
+    pc.currency,
+    pc.start_price,
+    pc.duration,
+    pc.target1,
+    pc.target2,
+    pc.target3,
+    pc.stop,
+    pc.direction,
+    pc.score,
+    pc.finished,
+    pc.created_at,
+    pc.updated_at,
+    m.id as "moniest_id",
+    m.bio,
+    m.description,
+    m.score as "moniest_score",
+    u.id as "user_id",
+    u.name,
+    u.surname,
+    u.username,
+    u.email_verified,
+    u.location,
+    u.created_at,
+    u.updated_at,
+    COALESCE (
+        (
+            SELECT "image"."link"
+            FROM "image"
+            WHERE "image".user_id = u.id
+                AND "image"."type" = 'profile_photo'
+        ),
+        ''
+    ) AS "profile_photo_link",
+    COALESCE (
+        (
+            SELECT "image"."thumbnail_link"
+            FROM "image"
+            WHERE "image".user_id = u.id
+                AND "image"."type" = 'profile_photo'
+        ),
+        ''
+    ) AS "profile_photo_thumbnail_link",
+    COALESCE (
+        (
+            SELECT "image"."link"
+            FROM "image"
+            WHERE "image".user_id = u.id
+                AND "image"."type" = 'background_photo'
+        ),
+        ''
+    ) AS "background_photo_link",
+    COALESCE (
+        (
+            SELECT "image"."thumbnail_link"
+            FROM "image"
+            WHERE "image".user_id = u.id
+                AND "image"."type" = 'background_photo'
+        ),
+        ''
+    ) AS "background_photo_thumbnail_link"
+FROM "post_crypto" AS pc
+    INNER JOIN "user_subscription" AS us ON pc.moniest_id = us.moniest_id
+    AND us.user_id = $1
+    AND pc.duration < now()
+    INNER JOIN "moniest" as m ON pc.moniest_id = m.id
+    INNER JOIN "user" as u ON m.user_id = u.id
+ORDER BY pc.duration DESC;
