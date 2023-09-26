786c12664547318f5696b0a9e56c3cbdd3d440a8
ParvinEyvazov <pervin.eyvazov1999@gmail.com>
2023-09-27 02:18:33 +0400

    feat: financial metrics added on metrics endpoint

--- model/admin.go
+++ model/admin.go
@@ -3,10 +3,17 @@ package model
 import db "github.com/moniesto/moniesto-be/db/sqlc"
 
 type MetricsResponse struct {
-	UserMetrics    db.UserMetricsRow    `json:"user_metrics"`
-	PostMetrics    db.PostMetricsRow    `json:"post_metrics"`
-	PaymentMetrics db.PaymentMetricsRow `json:"payment_metrics"`
-	PayoutMetrics  db.PayoutMetricsRow  `json:"payout_metrics"`
+	UserMetrics      db.UserMetricsRow    `json:"user_metrics"`
+	PostMetrics      db.PostMetricsRow    `json:"post_metrics"`
+	PaymentMetrics   db.PaymentMetricsRow `json:"payment_metrics"`
+	PayoutMetrics    db.PayoutMetricsRow  `json:"payout_metrics"`
+	FinancialMetrics FinancialMetrics     `json:"financial_metrics"`
+}
+
+type FinancialMetrics struct {
+	Payments        float64 `json:"payments"`
+	Profit          float64 `json:"profit"`
+	EstimatedProfit float64 `json:"estimated_profit"`
 }
 
 func NewMetricsResponse(
@@ -20,5 +27,12 @@ func NewMetricsResponse(
 		PostMetrics:    postMetrics,
 		PaymentMetrics: paymentMetrics,
 		PayoutMetrics:  payoutMetrics,
+		FinancialMetrics: FinancialMetrics{
+			Payments: paymentMetrics.SuccessPaymentAmount,
+			Profit: (payoutMetrics.SuccessPayoutsAmount - payoutMetrics.SuccessPayoutsAmountAfterCut) +
+				(payoutMetrics.RefundPayoutsAmount - payoutMetrics.RefundPayoutsAmountAfterCut),
+			EstimatedProfit: (payoutMetrics.SuccessPayoutsAmount - payoutMetrics.SuccessPayoutsAmountAfterCut) +
+				(payoutMetrics.RefundPayoutsAmount - payoutMetrics.RefundPayoutsAmountAfterCut) + (payoutMetrics.PendingPayoutsAmount - payoutMetrics.PendingPayoutsAmountAfterCut),
+		},
 	}
 }
